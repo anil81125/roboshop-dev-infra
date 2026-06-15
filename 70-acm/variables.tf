2891560c6d6a49160639a3695b7a94f820669525
anilkumar chigilipalli <anilkumarchigilipalli18@gmail.com>
2026-06-16 01:19:56 +0530

    updated

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "linakalinga.space"
+}
+
+variable "zone_id" {
+    default = "Z0630174BNY3JWPLU9E5"
+}
---
+++ 91-cloudfront/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0630174BNY3JWPLU9E"
+}
+
+variable "domain_name" {
+    default = "linakalinga.space"
+}
