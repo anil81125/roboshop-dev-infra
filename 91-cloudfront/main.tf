cd39f10a73e4dc969b2930647c5a9ab4fc1163ea
anilkumar chigilipalli <anilkumarchigilipalli18@gmail.com>
2026-06-17 07:44:13 +0530

    updated

--- 91-cloudfront/main.tf
+++ 91-cloudfront/main.tf
@@ -72,7 +72,7 @@ resource "aws_cloudfront_distribution" "roboshop" {
 
 resource "aws_route53_record" "cdn" {
   zone_id = var.zone_id
-  name    = "${var.domain_name}" # dev.daws86s.fun
+  name    = "${var.environment}.${var.domain_name}" # dev.daws86s.fun
   type    = "A"
   allow_overwrite = true
 
